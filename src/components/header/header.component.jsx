407fd2999d9772d808f8597ab4293f0ce88247e5
Daniel Dawidowski <dvds1987@gmail.com>
2019-10-23 15:28:49 +0200

    Adding Cart Component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -5,6 +5,7 @@ import { connect } from 'react-redux'
 import { auth } from '../../firebase/firebase.utils'
 
 import { ReactComponent as Logo } from '../../assets/crown.svg'
+import CartIcon from '../../components/cart-icon/cart-icon.component'
 
 import './header.styles.scss'
 
@@ -21,6 +22,7 @@ const Header = ({ currentUser }) => (
                 :
                 <Link className='option' to='/signin'>SIGN IN</Link>
             }
+            <CartIcon />
         </div>
     </div>
 )
