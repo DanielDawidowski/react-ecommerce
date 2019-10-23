14ca39c7b84e7ab19b8cde4a615fa929eca57181
Daniel Dawidowski <dvds1987@gmail.com>
2019-10-23 18:35:21 +0200

    Adding 'createStructuredSelector to all components with  mapStateToProps'

--- src/components/cart-icon/cart-icon.component.js
+++ src/components/cart-icon/cart-icon.component.js
@@ -6,6 +6,7 @@ import { createStructuredSelector } from 'reselect'
 import { toggleCartHidden } from '../../redux/cart/cart.actions.js'
 import { selectCartItemsCount } from '../../redux/cart/cart.selectors.js'
 
+
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg'
 
 import './cart-icon.styles.scss'
