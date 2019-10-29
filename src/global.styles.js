9ac30729ee57acc3eb5797b4a36098d2875b1141
Daniel Dawidowski <dvds1987@gmail.com>
2019-10-29 15:47:19 +0100

    stylings

--- src/global.styles.js
+++ src/global.styles.js
@@ -4,6 +4,7 @@ export const GlobalStyle = createGlobalStyle`
     body {
         font-family: 'Open Sans Condensed';
         padding: 20px 60px;
+        background-color: #F0F0C9;
 
         @media screen and (max-width: 800px){
             padding: 10px;
